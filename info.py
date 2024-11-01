c8465f182cec11e2217e453627d7fd28d2ebb318
flashpoint55 <koyeb0125@gmail.com>
2024-11-01 17:35:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://effective-doris-ultronbot-51b957d6.koyeb.app/")
+URL = environ.get("URL", "peaceful-cherianne-patrick637-24b931cc.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
